ed36951a0b6d7f45fabde80e536522aebf4cb74a
meet shah <meet.shah@dnata.com>
2020-05-19 23:17:33 +0400

    Added  AppDynamics Certified Associate Performance Analyst

---
+++ AWS Training & Certification - Certificate of Completion_files/arrayHelper.js
@@ -0,0 +1,2 @@
+/** Copyright 2020 Amazon.com Inc. or its affiliates. All Rights Reserved. **/
+define([],function(){var a;return a={},a.isNullOrEmpty=function(a){return void 0===a||null===a||0===a.length},a});
---
+++ AWS Training & Certification - Certificate of Completion_files/kikuEnvironment.js
@@ -0,0 +1,2 @@
+/** Copyright 2020 Amazon.com Inc. or its affiliates. All Rights Reserved. **/
+var CDNBasePath = 'https://cdn.aws.training/kiku/prod/Kiku_2019287',MarketoAccountId="112-TZM-766";
